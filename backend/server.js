77dc476e1a98e25ab07b45c4b6a084ffd74c44c3
Shashwat Mishra <shashwatmishra466@gmail.com>
2024-12-03 22:28:45 +0530

    fix: cores issue

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,24 @@ import 'dotenv/config'
 import foodRouter from "./routes/foodRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
-
+const allowedOrigins = ['https://food-delivery-frontend-b7q4.onrender.com'];
 //app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // If you need to send cookies or auth headers
+  }));
 
 
 //DB connection
